00cc98fca74cd760a644cfa2b1cd8bb71e4ffb54
zorobests <166844400+zorobests@users.noreply.github.com>
2024-04-13 17:21:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ class Config(object):
     DB_NAME  = os.environ.get("DB_NAME","SnowEncoderBot") 
 
     # Other Configs 
-    ADMIN = int(os.environ.get("ADMIN", "1327989718 2128197222")) # ⚠️ Required
+    ADMIN = int(os.environ.get("ADMIN", "1327989718")) # ⚠️ Required
     LOG_CHANNEL = int(os.environ.get('LOG_CHANNEL', '-1001896354874')) # ⚠️ Required
     BOT_UPTIME = BOT_UPTIME  = time.time()
     START_PIC = os.environ.get("START_PIC", "https://graph.org/file/15e82d7e665eccc8bd9c5.jpg")
